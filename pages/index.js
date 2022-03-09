26db29596b3dd07bf3adb23517a0d0d8b9c1edb8
Lucas <ahdsr>
2022-03-09 01:22:14 -0500

    Updated hero

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
             <LazyMotion features={domAnimation}>
               <m.div initial='initial' animate='enter' exit='exit'>
                 <m.main variants={fade} className='pb-4'>
-                  {/* <div
+                  <div
                     style={{
                       position: 'relative',
                       width: '100%',
@@ -55,8 +55,8 @@ export default function Home() {
                       layout='fill'
                       objectFit='cover'
                     />
-                  </div> */}
-                  <div className='player-wrapper '>
+                  </div>
+                  {/* <div className='player-wrapper '>
                     <ReactPlayer
                       url='https://youtu.be/oCMQEDL_iBU'
                       className='react-player'
@@ -72,33 +72,9 @@ export default function Home() {
                         },
                       }}
                     />
-                  </div>
+                  </div> */}
                   <div className='bg-slate-900 mx-0 sm:mx-6 md:mx-24 lg:mx-24 xl:mx-40 h-36 z-10 relative -mt-20 grid place-items-center rounded-br-3xl rounded-bl-3xl'>
                     <p className='block p-8 text-white '>CLIENT LIST BOX</p>
-                  </div>
-                  <div className='pt-48 pb-36 md:pt-56 md:pb-44 bg-gradient-to-bl from-customblue via-customblue to-customblue text-white -mt-36 z-0  '>
-                    <div className='md:flex mx-2 px-4 md:mx-12 lg:mx-24 xl:mx-80  '>
-                      <div className='md:item md:w-1/2 h-auto content md:pr-16  '>
-                        <h1>
-                          Plan, discover, design and iterate ... and iterate and
-                          iterate.
-                        </h1>
-                      </div>
-                      <div className='md:item md:w-1/2 h-auto content-sub'>
-                        <p>
-                          As a designer, I've been lucky enough to be exposed to
-                          a number of employers, teams and projects. Each has
-                          been a a unique and rewarding experience, but some
-                          things always remain common.
-                        </p>
-                        <p>
-                          Irregardless of where I work, the process is very
-                          similar. Plan, discover, design and iterate.
-                        </p>
-                        <p>Copy</p>
-                        <p>Copy</p>
-                      </div>
-                    </div>
                   </div>
 
                   <div className='pt-48 pb-36 md:pt-56 md:pb-44 bg-gradient-to-bl from-customblue via-customblue to-customblue text-white -mt-36 z-0  '>
